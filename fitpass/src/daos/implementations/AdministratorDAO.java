3620b0981a11d2ca7ae7e290b17a6e0259599e05
Miloš Zeljko <zeljko.ra24.2019@uns.ac.rs>
2022-06-15 19:12:30 +0200

    implemented CRUD for administrators

---
+++ fitpass/src/daos/implementations/AdministratorDAO.java
@@ -0,0 +1,67 @@
+package daos.implementations;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import beans.models.Administrator;
+import daos.interfaces.IDAO;
+import repositories.interfaces.IRepository;
+
+public class AdministratorDAO implements IDAO<Administrator> {
+
+	private Map<String, Administrator> administrators = new HashMap<String, Administrator>();
+	private IRepository<Administrator> administratorRepository;
+	
+	public AdministratorDAO(IRepository<Administrator> administratorRepository) {
+		super();
+		this.administratorRepository = administratorRepository;
+		administrators = administratorRepository.load();
+	}
+
+	@Override
+	public Collection<Administrator> getAll() {
+		return administrators.values();
+	}
+
+	@Override
+	public Administrator get(String id) {
+		return administrators.containsKey(id) ? administrators.get(id) : null;
+	}
+
+	@Override
+	public Administrator create(Administrator administrator) {
+		long maxId = -1;
+		for (String id : administrators.keySet()) {
+			long idNum = Long.parseLong(id);
+			if (idNum > maxId) {
+				maxId = idNum;
+			}
+		}
+		maxId++;
+		
+		administrator.setId(maxId);
+		administrators.put(String.valueOf(administrator.getId()), administrator);
+		administratorRepository.save(administrators);
+		return administrator;
+	}
+
+	@Override
+	public boolean update(Administrator administrator) {
+		if(administrators.put(String.valueOf(administrator.getId()), administrator) != null) {
+			administratorRepository.save(administrators);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean delete(String id) {
+		if(administrators.remove(id) != null) {
+			administratorRepository.save(administrators);
+			return true;
+		}
+		return false;
+	}
+
+}

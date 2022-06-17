c203bd06b71c2238fba26b5b8495d4987abf8888
Miloš Zeljko <zeljko.ra24.2019@uns.ac.rs>
2022-06-17 19:33:08 +0200

    implemented login and register forms

--- fitpass/WebContent/js/app.js
+++ fitpass/WebContent/js/app.js
@@ -1,15 +1,43 @@
-const header = { template: '<header></header>' }
-const footer = { template: '<footer></footer>' }
+const MyHeader = { template: '<my-header></my-header>' }
+const MyFooter = { template: '<my-footer></my-footer>' }
+const SportsFacilities = { template: '<sports-facilities></sports-facilities>' }
 
 const router = new VueRouter({
 	mode: 'hash',
 	  routes: [
-		{ path: '/', component: header},
-	    { path: '/footer', component: footer}
+		{path: '/', component: SportsFacilities}
 	  ]
 });
 
 var app = new Vue({
 	router,
-	el: '#app'
+	el: '#app',
+	vuetify: new Vuetify(),
+	data: {
+		mode: "GUEST",
+		appBarLinks: [
+			'Home',
+			'Sport Facilities',
+			'Programs',
+			'Trainers',
+		],
+		drawer: false,
+		group: null,
+		registerDialog: false,
+		loginDialog: false,
+		activePicker: null,
+		date: null,
+		menu: false,
+        radios: null,
+	},
+	watch: {
+		menu(val) {
+			val && setTimeout(() => (this.activePicker = 'YEAR'))
+		},
+	},
+	methods: {
+		save(date) {
+			this.$refs.menu.save(date)
+		},
+	},
 });

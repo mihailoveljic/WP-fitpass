b9f91f30c485119c32ca56fc12a60c4b048ef544
Miloš Zeljko <zeljko.ra24.2019@uns.ac.rs>
2022-06-19 01:39:42 +0200

    implemented design for sports facility data card

--- fitpass/WebContent/components/sports-facilities.js
+++ fitpass/WebContent/components/sports-facilities.js
@@ -1,13 +1,82 @@
 Vue.component("sports-facilities", {
 	data: function () {
 		    return {
-		     
+				
 		    }
 	},
-	template: ` 
+	template: 
+` 
 	<div>
-		
-    </div>
+		<v-card width="800px" height="252px" class="mx-auto mt-8" outlined  rounded="8" hover>
+			<v-row class="d-flex flex-row justify-space-between">
+				<v-col cols="5">
+					<v-img class="ma-4" src="data/img/sports-facilities/teretana1.jpg" height="220px" width="220px" dark></v-img>
+				</v-col>
+				<v-col cols="7" align-self="center">
+					<div class="text-h4 gray--text text-center mt-2">
+						Fitnes centar Flex
+					</div>
+					<div class="text-body-1 gray--text text-center text-center">
+						Teretana
+					</div>
+					<v-divider class="my-2"></v-divider>
+					<div class="text-body-1 gray--text text-center">
+						Sutjeska 2, Novi Sad, 21000
+					</div>
+					<div class="text-body-1 gray--text text-center green--text">
+						08:00 - 21:00   |   Otvoreno
+					</div>
+					<v-rating color="primary" class="text-center" half-increments length="5" size="26" value="4.5" readonly></v-rating>
+				</v-col>
+			</v-row>
+		</v-card>
+		<v-card width="800px" height="252px" class="mx-auto mt-8" outlined  rounded="8" hover>
+			<v-row class="d-flex flex-row justify-space-between">
+				<v-col cols="5">
+					<v-img class="ma-4" src="data/img/sports-facilities/teretana1.jpg" height="220px" width="220px" dark></v-img>
+				</v-col>
+				<v-col cols="7" align-self="center">
+					<div class="text-h4 gray--text text-center mt-2">
+						Fitnes centar Flex
+					</div>
+					<div class="text-body-1 gray--text text-center text-center">
+						Teretana
+					</div>
+					<v-divider class="my-2"></v-divider>
+					<div class="text-body-1 gray--text text-center">
+						Sutjeska 2, Novi Sad, 21000
+					</div>
+					<div class="text-body-1 gray--text text-center green--text">
+						08:00 - 21:00   |   Otvoreno
+					</div>
+					<v-rating color="primary" class="text-center" half-increments length="5" size="26" value="4.5" readonly></v-rating>
+				</v-col>
+			</v-row>
+		</v-card>
+		<v-card width="800px" height="252px" class="mx-auto mt-8" outlined  rounded="8" hover>
+			<v-row class="d-flex flex-row justify-space-between">
+				<v-col cols="5">
+					<v-img class="ma-4" src="data/img/sports-facilities/teretana1.jpg" height="220px" width="220px" dark></v-img>
+				</v-col>
+				<v-col cols="7" align-self="center">
+					<div class="text-h4 gray--text text-center mt-2">
+						Fitnes centar Flex
+					</div>
+					<div class="text-body-1 gray--text text-center text-center">
+						Teretana
+					</div>
+					<v-divider class="my-2"></v-divider>
+					<div class="text-body-1 gray--text text-center">
+						Sutjeska 2, Novi Sad, 21000
+					</div>
+					<div class="text-body-1 gray--text text-center green--text">
+						08:00 - 21:00   |   Otvoreno
+					</div>
+					<v-rating color="primary" class="text-center" half-increments length="5" size="26" value="4.5" readonly></v-rating>
+				</v-col>
+			</v-row>
+		</v-card>
+	</div>
 `
 	, 
 	methods : {

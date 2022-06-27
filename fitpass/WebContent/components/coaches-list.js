db2b6a9ab6dda8078810cd629b3c08dbef2d7d0d
Mihailo Veljic <veljic.ra31.2019@uns.ac.rs>
2022-06-27 23:32:18 +0200

    administrator now can register new coach v1

--- fitpass/WebContent/components/coaches-list.js
+++ fitpass/WebContent/components/coaches-list.js
@@ -38,8 +38,8 @@ Vue.component("coaches-list", {
 					<v-row justify="center">
 						<v-dialog v-model="createDialog" persistent max-width="600px">
 							<template v-slot:activator="{ on, attrs }">
-								<v-btn dark v-bind="attrs" v-on="on" class="mx-8 d-none d-lg-flex"
-									outlined color="indigo" centered x-large>
+								<v-btn v-bind="attrs" v-on="on" color="primary" class="ma-0 d-none d-lg-flex""
+								 centered x-large>
 									Add Coach
 								</v-btn>
 							</template>
@@ -113,7 +113,7 @@ Vue.component("coaches-list", {
 									<v-btn color="blue darken-1" text @click="createDialog = false">
 										Cancel
 									</v-btn>
-									<v-btn color="blue darken-1" text @click="register">
+									<v-btn color="blue darken-1" text @click="register(); reloadPage()">
 										Register
 									</v-btn>
 								</v-card-actions>
@@ -192,15 +192,17 @@ Vue.component("coaches-list", {
               .then(response => {
 				this.userToken = response.data
               	if(this.userToken == null) return;
-	            sessionStorage.setItem('userToken', JSON.stringify(this.userToken));
-	            this.mode= this.userToken.role;
 	            this.createDialog = false;
+	            alert("New coach successfuly added!")
               }
               )
               .catch(error => {
                     alert(error.message + " GRESKA");
                     });
 		},
+		reloadPage(){
+    		window.location.reload();
+  		}
 	},
 	watch: {
 		menu(val) {

113670c0b2fcfc11f286e96f047f561212b94949
Mihailo Veljic <veljic.ra31.2019@uns.ac.rs>
2022-07-03 00:17:43 +0200

    Functionality is working, but ofc bugs with dizain and vuetify
    Dodavanje radi, ali jebeni bagovi sa slikom stvarno ne znam sta se desava

--- fitpass/src/controllers/implementations/TrainingController.java
+++ fitpass/src/controllers/implementations/TrainingController.java
@@ -1,5 +1,12 @@
 package controllers.implementations;
 
+import java.io.File;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -8,13 +15,17 @@ import javax.servlet.ServletContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import beans.dtos.TrainingDTO;
 import beans.models.Coach;
@@ -28,6 +39,8 @@ import services.interfaces.ITrainingService;
 @Path("/TrainingController")
 public class TrainingController {
 
+		public static int MAX_SIZE_IN_MB = 5;
+		
 		@Context
 		ServletContext ctx;
 		
@@ -121,15 +134,57 @@ public class TrainingController {
 		}
 		
 		@POST
+		@Path("/uploadImage")
+	    @Consumes({"image/jpeg", "image/png"})
+	    public Response uploadImage(InputStream in, @HeaderParam("Content-Type") String fileType, @HeaderParam("Content-Length") long fileSize) throws IOException {
+	        
+	        // Make sure the file is not larger than the maximum allowed size.
+	        if (fileSize > 1024 * 1024 * MAX_SIZE_IN_MB) {
+	            throw new WebApplicationException(Response.status(Status.BAD_REQUEST).entity("Image is larger than " + MAX_SIZE_IN_MB + "MB").build());
+	        }
+	        
+	        // Generate a random file name based on the current time.
+	        // This probably isn't 100% safe but works fine for this example.
+	        String fileName = "" + System.currentTimeMillis();
+
+	        if (fileType.equals("image/jpeg")) {
+	            fileName += ".jpg";
+	        } else {
+	            fileName += ".png";
+	        }
+	        
+	        File f = new File(ctx.getRealPath("data\\img\\trainings\\" + fileName));
+	        System.out.println(f.getAbsolutePath());
+
+	        // Copy the file to its location.
+	        Files.copy(in, f.toPath(), StandardCopyOption.REPLACE_EXISTING);
+	        in.close();
+	        
+	        // Return a 201 Created response with the appropriate Location header.
+	        return Response.status(Status.CREATED).location(URI.create("/" + fileName)).build();
+	    }
+		
+		@POST
 		@Path("/")
 		@Consumes(MediaType.APPLICATION_JSON)
 		@Produces(MediaType.APPLICATION_JSON)
-		public TrainingDTO create(Training training) {
-			//ITrainingService trainingService = (ITrainingService) ctx.getAttribute("TrainingService");
+		public Training create(TrainingDTO trainingDTO) {
+			ITrainingService trainingService = (ITrainingService) ctx.getAttribute("TrainingService");
 			//Training training2 = trainingService.create(training);
 			//TrainingDTO trainingDTO = new TrainingDTO();
 			//return trainingService.transformFromTrainingToTrainingDTO(training2, trainingDTO);
-			return null;
+			Training training = new Training();
+			training.setId(trainingDTO.getId());
+			training.setName(trainingDTO.getName());
+			training.setTrainingTypeId(trainingDTO.getTrainingType().getId());
+			training.setSportsFacilityId(trainingDTO.getSportsFacilityId());
+			training.setDuration(trainingDTO.getDuration());
+			training.setCoachId(trainingDTO.getCoach().getId());
+			training.setDescription(trainingDTO.getDescription());
+			training.setImage(ctx.getContextPath() + "\\data\\img\\trainings\\" + trainingDTO.getImage());
+			training.setAdditionalPrice(trainingDTO.getAdditionalPrice());
+			training.setIsDeleted(false);
+			return trainingService.create(training);
 		}
 		
 		@PUT
